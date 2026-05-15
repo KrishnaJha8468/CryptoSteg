20c771845759180cbf230ba83c73f697ab0733cd
KrishnaJha8468 <krishna.jhhha@gmail.com>
2026-05-15 22:34:17 +0545

    v0.2: Add AES-256 encryption class skeleton

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-﻿print("CryptoSteg v0.1 - Loading...")
-print("Steganography Tool Starting...")
+﻿import hashlib
+from Crypto.Cipher import AES
+
+class AES256:
+    def __init__(self):
+        self.key = None
+    
+    def set_password(self, password):
+        self.key = hashlib.sha256(password.encode()).digest()
+        print(f"Password set: {'*' * len(password)}")
+
+print("CryptoSteg v0.2 - Encryption module added")
